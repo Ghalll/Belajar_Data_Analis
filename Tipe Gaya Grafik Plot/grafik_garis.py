09e187a25ade9c3682dfbb283a39a78265f8c586
Ghaly risyadi <126685634+Ghalll@users.noreply.github.com>
2025-12-15 00:35:22 +0700

    Add files via upload

---
+++ Tipe Gaya Grafik Plot/grafik_garis.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+data = pd.DataFrame({
+    "Year": [2018, 2019, 2020, 2021, 2022],
+    "Phoenix Police Dept": [1200, 1300, 1250, 1400, 1500],
+    "Los Angeles Police Dept": [2000, 2100, 2200, 2300, 2400],
+    "Chicago Police Dept": [1800, 1750, 1700, 1650, 1600]
+})
+
+plt.style.use("ggplot")
+
+plt.plot(data["Year"], data["Phoenix Police Dept"],
+         label="Phoenix", color="DarkCyan")
+
+plt.plot(data["Year"], data["Los Angeles Police Dept"],
+         label="Los Angeles", linestyle=":")
+
+plt.plot(data["Year"], data["Chicago Police Dept"],
+         label="Chicago", marker="s")
+
+plt.xlabel("Year")
+plt.ylabel("Number of Incidents")
+plt.title("Police Department Incident Reports (2018-2022)")
+plt.legend()
+plt.show()
